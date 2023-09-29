1ac99bf02d1282af0e03e561e78291cdfdc9ba62
Jon Cursi <joncursi@gmail.com>
2023-09-29 01:21:16 -0400

    feat: Added more content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,20 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jon Loves Michelle",
+  applicationName: "Jon Loves Michelle",
+  authors: [{ name: "Jon Cursi", url: "https://github.com/joncursi" }],
+  creator: "Jon Cursi",
   description: "Jon loves Michelle very very very much.",
+  icons: [
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+    { rel: "icon", url: "/icon.png" },
+  ],
+  keywords: ["Jon", "Loves", "Michelle"],
+  publisher: "Jon Cursi",
+  robots: { index: true, follow: true },
+  themeColor: "#FFFFFF",
+  title: "Jon Loves Michelle",
+  viewport: { width: "device-width", initialScale: 1 },
 };
 
 export default function RootLayout({
